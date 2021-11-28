06cc60d430664a7f3952f7f8de83723a2e659ad6
Laura Toribio <lsancheztoribio@hotmail.com>
2021-11-28 20:17:49 +0100

    Iteration#1

---
+++ iteration1.js
@@ -0,0 +1,23 @@
+//Haz un bucle y muestra por consola todos aquellos valores 
+//del array que incluyan la palabra "Camiseta".
+
+const products = [
+  "Camiseta de Pokemon",
+  "Pantalón coquinero",
+  "Gorra de gansta",
+  "Camiseta de Basket",
+  "Cinrurón de Orión",
+  "AC/DC Camiseta",
+];
+
+function FindWord(param) {
+
+for (let i = 0; i < param.length; i++ ) {
+    if (param[i].includes("Camiseta")) {
+        console.log(param[i])
+    }
+}
+}
+
+FindWord(products)
+
