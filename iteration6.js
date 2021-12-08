89294ecffb4f969e885c558542164f96eab7c479
Laura Toribio <lsancheztoribio@hotmail.com>
2021-12-08 19:01:27 +0100

    Iteration#6 & #7

--- iteration6.js
+++ iteration6.js
@@ -10,24 +10,18 @@ const toys = [
     {id: 40, name: 'El gato felix'}
     ]
 
-    let palabra = "gato"
-
-//    for(let propreties of toys) {
-//        let i = 0
-//        if(propreties[i].name.indexOf(palabra)) {
-//         toys.splice(i,1)
-//        }
-//    }
-    
-//     console.log(toys)
 
 
-
-for (let property of toys){
-    for(let i = 0; i < toys.length; i++) {
-    if (property[i].indexOf(palabra) == true) {
-        toys.splice(i,1)
+ for(let properties of toys) {
+     if(properties.name.indexOf("gato")) {
+      toys.splice(properties, 1)
      }
-   }
-}
-console.log(toys)
+ }
+  console.log(toys)
+
+
+
+
+
+
+
