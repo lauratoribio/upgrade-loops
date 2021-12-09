4fe2da6a0b766002fe0f248eadb4f54a630b8de0
Laura Toribio <lsancheztoribio@hotmail.com>
2021-12-09 19:58:18 +0100

    Iteration#6 solved

--- iteration6.js
+++ iteration6.js
@@ -11,11 +11,19 @@ const toys = [
     ]
 
 
+let toyId = []
 
- for(let properties of toys) {
-     if(properties.name.indexOf("gato")) {
-      toys.splice(properties, 1)
-     }
+for(let properties of toys) {
+    if(properties.name.includes("gato")) {
+     toyId.push(properties.id)
+    }
+}
+ console.log(toyId)
+
+
+
+ for(let id of toyId) {
+      toys.splice(toys.indexOf(id),1)
  }
   console.log(toys)
 
